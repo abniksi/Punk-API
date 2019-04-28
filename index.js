45786f53c5f1a07ccaca761c815627182dff9857
[Braden Niksich] <[abniksi@ilstu.edu]>
2019-04-28 12:11:23 -0500

    Added search functionality

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-$.getJSON("https://api.punkapi.com/v2/beers", function(data) {
-  let userInput = document.getElementById("search").value;
-  console.log(userInput);
-  const beerSearch = "https://api.punkapi.com/v2/beers?beer_name=" + userInput;
-  console.log(beerSearch);
+document.getElementById("searchBtn").addEventListener("click", () => {
+  let removeContent = $(".container");
+  removeContent.remove();
+  let searchTerm = document.getElementById("search").value;
+  $.getJSON(
+    `https://api.punkapi.com/v2/beers?beer_name=${searchTerm}`,
+    function(data) {
+      let x = 0;
+      for (let i = 0; i < data.length; i++) {
+        let beerHTML = `
+            <div class="container">
+                <div class="imageContainer">
+                    <img class="icon" src='${data[x].image_url}'/>
+                </div>
+                <h6 class="name">${data[x].name}</h6>
+                <p class="description">${data[x].description}</p>
+                <p class="continue">...</p>
+        
+                <p class="level">Alcohol Level: ${data[x].abv}%</p>
+            </div> `;
 
-  let x = 0;
-  for (let i = 0; i < data.length; i++) {
-    let beerHTML = `
-    <div class="container">
-        <div class="imageContainer">
-            <img class="icon" src = "${data[x].image_url}"/>
-        </div>
-        <h6 class="name">${data[x].name}</h6>
-        <p class="description">${data[x].description}</p>
-        <p class="continue">...</p>
-
-        <p class="level">Alcohol Level: ${data[x].abv}%</p>
-    </div> `;
-
-    $("#beerDisplay").append(beerHTML);
-
-    x++;
-  }
+        x++;
+        $("#beerDisplay").append(beerHTML);
+      }
+    }
+  );
 });
